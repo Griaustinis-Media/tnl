e6942ecba236af4d0e1dce2c375a6ae0d704a054
Šarūnas Navickas <zaibacu@gmail.com>
2025-12-06 00:56:34 +0200

    Updates for druid sink

--- leng/src/gm/sink/druid.clj
+++ leng/src/gm/sink/druid.clj
@@ -1,16 +1,21 @@
 (ns gm.sink.druid
   (:require [gm.sink.core :as sink]
             [clj-http.client :as http]
-            [cheshire.core :as json]))
+            [cheshire.core :as json]
+            [cheshire.generate :as json-gen]
+            [clojure.tools.logging :as log])
+  (:import [java.time Instant]
+           [java.time.format DateTimeFormatter]))
+
+;; Add custom JSON encoder for java.time.Instant
+(json-gen/add-encoder java.time.Instant
+                      (fn [instant jsonGenerator]
+                        (.writeString jsonGenerator (.toString instant))))
 
 (defrecord DruidAdapter [base-url datasource auth-config])
 
 (defn- build-http-options
-  "Build HTTP options with optional authentication.
-
-        Supports:
-        - Basic Auth: {:type :basic :username \"user\" :password \"pass\"}
-        - Bearer Token: {:type :bearer :token \"jwt-token\"}"
+  "Build HTTP options with optional authentication"
   [adapter content-type]
   (let [base-opts {:content-type content-type}
         auth (:auth-config adapter)]
@@ -23,32 +28,76 @@
         base-opts)
       base-opts)))
 
+(defn- convert-timestamps
+  "Convert all timestamp fields to ISO-8601 strings"
+  [record]
+  (into {}
+        (map (fn [[k v]]
+               [k (cond
+                    (instance? java.time.Instant v)
+                    (.toString v)
+
+                    (instance? java.util.Date v)
+                    (.format (java.text.SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'") v)
+
+                    (instance? java.sql.Timestamp v)
+                    (.toString (.toInstant v))
+
+                    :else v)])
+             record)))
+
 (defn- records->druid-format
-  "Convert records to Druid ingestion format"
+  "Convert records to Druid ingestion format using index_parallel"
   [records timestamp-column datasource]
-  {:type "index_parallel"
-   :spec {:dataSchema {:dataSource datasource
-                       :timestampSpec {:column (name timestamp-column)
-                                       :format "auto"}
-                       :dimensionsSpec {:dimensions (keys (first records))}
-                       :metricsSpec []}
-          :ioConfig {:type "inline"
-                     :inputSource {:type "inline"
-                                   :data (json/generate-string records)}}
-          :tuningConfig {:type "index_parallel"}}})
+  (let [converted-records (map convert-timestamps records)
+        dimensions (remove #{(keyword timestamp-column)}
+                           (keys (first converted-records)))]
+    {:type "index_parallel"
+     :spec {:ioConfig {:type "index_parallel"
+                       :inputSource {:type "inline"
+                                     :data (clojure.string/join "\n"
+                                                                (map json/generate-string converted-records))}
+                       :inputFormat {:type "json"}}
+            :tuningConfig {:type "index_parallel"
+                           :partitionsSpec {:type "dynamic"}}
+            :dataSchema {:dataSource datasource
+                         :timestampSpec {:column (name timestamp-column)
+                                         :format "auto"}
+                         :dimensionsSpec {:dimensions (mapv name dimensions)}
+                         :granularitySpec {:type "uniform"
+                                           :segmentGranularity "DAY"
+                                           :queryGranularity "NONE"
+                                           :rollup false}}}}))
 
 (extend-type DruidAdapter
   sink/SinkAdapter
 
   (insert [this table records]
-    (let [task-spec (records->druid-format records :timestamp table)
+    (let [task-spec (records->druid-format records :event_time table)
           url (str (:base-url this) "/druid/indexer/v1/task")
           http-opts (merge (build-http-options this :json)
-                           {:body (json/generate-string task-spec)})
-          response (http/post url http-opts)]
-      (json/parse-string (:body response) true)))
+                           {:body (json/generate-string task-spec)
+                            :throw-exceptions false})]
+      (log/info "Submitting Druid ingestion task for" (count records) "records")
+      (try
+        (let [response (http/post url http-opts)]
+          (if (= 200 (:status response))
+            (do
+              (log/info "Druid task submitted successfully")
+              (json/parse-string (:body response) true))
+            (do
+              (log/error "Druid ingestion failed with status:" (:status response))
+              (log/error "Response body:" (:body response))
+              (log/error "Task spec:" (json/generate-string task-spec {:pretty true}))
+              (throw (ex-info "Druid ingestion failed"
+                              {:status (:status response)
+                               :body (:body response)
+                               :task-spec task-spec})))))
+        (catch Exception e
+          (log/error "Exception during Druid ingestion:" (.getMessage e))
+          (throw e)))))
 
-  (update-records [this table conditions updates]    ; RENAMED
+  (update-records [this table conditions updates]
     (throw (ex-info "Druid doesn't support in-place updates"
                     {:operation :update})))
 
@@ -68,7 +117,9 @@
      :schema schema})
 
   (batch-insert [this table records batch-size]
-    (sink/insert this table records))
+                           ;; Druid does batch ingestion by default, so just call insert
+    (doseq [batch (partition-all batch-size records)]
+      (sink/insert this table batch)))
 
   (transaction [this operations]
     (throw (ex-info "Druid doesn't support transactions"
